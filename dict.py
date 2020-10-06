ff36b4575d5b631f0492895155097342d80aa97b
Prakash Rakshit <64463739+lucifer-shaw77@users.noreply.github.com>
2020-10-06 23:10:34 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,7 @@
+dict1=dict()
+range1=int(input("enter the no. of players: "))
+for i in range (range1):
+    name=input("enter the name of the player: ")
+    score=input("enter the score of the player:")
+    dict1[name]=score
+print(dict1)
